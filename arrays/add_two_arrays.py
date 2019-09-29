339aa43e2ee66399f0a39545306a2f54a09c4b69
RAMAN DWIVEDI <ramandwivedi20@gmail.com>
2019-09-29 10:36:16 +0530

    Update add_two_arrays.py

--- arrays/add_two_arrays.py
+++ arrays/add_two_arrays.py
@@ -1,3 +1,5 @@
+# https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/monk-and-welcome-problem/
+
 n = int(input())
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
